0bdc376e87419e5ad841335a056f9e5202ca2064
alex <alexander.tarler@gmail.com>
2024-01-16 15:57:15 +0100

    Started working on the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+// Import the CSS module<p className={styles.paragraph}>
+import styles from "../../styles/About.module.css";
 
 const About: React.FC = () => {
   return (
-    <div className="about-page">
-      <h1>About Me</h1>
-      <p>Welcome to my portfolio website!</p>
-      <p>
-        I am a passionate software developer with experience in web development.
-      </p>
-      <p>Feel free to explore my projects and get in touch with me.</p>
+    <div className={styles.aboutPage}>
+      <h1 className={styles.aboutHeader}>About Me</h1>
+      <section className={styles.intro}>
+        <h2 className={styles.subheader}>Alexander Tarler, Developer</h2>
+        <p className={styles.paragraph}>
+          Hello and welcome to my portfolio! I&apos;m Alexander Tarler, a
+          passionate and dedicated Developer...
+        </p>
+      </section>
+      <section className={styles.journey}>
+        <h2 className={styles.subheader}>My Journey into Development</h2>
+        <p className={styles.paragraph}>
+          My fascination with building things began with my first LEGO set...
+        </p>
+      </section>
+      <section className={styles.connect}>
+        <h2 className={styles.subheader}>Let&apos;s Connect</h2>
+        <p className={styles.paragraph}>
+          I believe in building solutions that make a difference...
+        </p>
+      </section>
     </div>
   );
 };
